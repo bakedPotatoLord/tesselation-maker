11bd3b4334efeb46bad924f8c4271364acfa058e
bakedpotatolord <jeffy9745@gmail.com>
2024-04-25 18:59:27 -0600

    fix canvas drawing

--- app.vue
+++ app.vue
@@ -1,50 +1,81 @@
-
 <template>
-  <canvas ref="canvas"></canvas>
+	<div>
+		<canvas ref="canvas"></canvas>
+	</div>
 </template>
 
-
 <script setup lang="ts">
-import tesselatonSpritesheet from './assets/tesselationSpritesheet.png'
+import { ref, onMounted } from 'vue';
+import tesselatonSpritesheet from './assets/tesselationSpritesheet.png';
 
-const width = 1600;
-const height = 800;
+type direction = 0 | 1 | 2 | 3
 
-onMounted(async() => {
-	await nextTick();
-  console.log("mounted");
+const canvas = ref<HTMLCanvasElement | null>(null);
 
+const img = new Image();
+img.src = tesselatonSpritesheet;
 
-  const canvas = ref<HTMLCanvasElement | null>(null);
-  console.log(canvas.value);
+const numTilesX = 16
+const numTilesY = 16
 
-  if (canvas.value) {
-    loadCanvas(canvas.value);
-  } else {
-    console.log("no canvas");
-  }
+const tileWidth = 32
+const tileHeight = 32
+const numRows = img.height / tileHeight
 
+const cw = numTilesX * tileWidth
+const ch = numTilesY * tileHeight
+
+console.log("rows", numRows)
+
+onMounted(async () => {
+	await new Promise<void>((res) => { img.onload = () => { res() } });
+	if (canvas.value) {
+		const ctx = canvas.value.getContext('2d');
+		canvas.value.width = cw;
+		canvas.value.height = ch;
+		draw(ctx)
+	} else {
+		console.error('Canvas element not found');
+	}
 });
 
+function draw(ctx: CanvasRenderingContext2D) {
+	ctx.clearRect(0, 0, cw, ch);
+	ctx.fillStyle = 'black';
+	ctx.fillRect(0, 0, cw, ch);
 
-function loadCanvas(canvas: HTMLCanvasElement) {
-	const ctx = canvas.getContext('2d');
-	const img = new Image();
-	img.hidden = true;
-	img.src = tesselatonSpritesheet;
+	for(let y = 0; y < numTilesY; y++) {
+		for(let x = 0; x < numTilesX; x++) {
+			ctx.filter = 'hue-rotate('+(Math.floor(Math.random() * 360))+'deg)'
+			drawTile(ctx, x, y, randDirection(), randTile());
+		}
+	}
 }
 
+function drawTile(ctx: CanvasRenderingContext2D, tileX: number, tileY: number, rot: direction,imgNum: number) {
+	ctx.save();
+	ctx.translate(tileWidth / 2, tileHeight / 2);
+	ctx.translate(tileX * tileWidth, tileY * tileHeight);
+	ctx.rotate(rot * Math.PI / 2);
+
+	ctx.drawImage(img,0,tileHeight * imgNum, tileWidth, tileHeight, -tileWidth / 2, -tileHeight / 2, tileWidth, tileHeight);
+
+	ctx.restore();
+}
+
+function randDirection() {
+	return <direction>Math.floor(Math.random() * 4);
+}
+
+function randTile() {
+	return Math.floor(Math.random() * numRows);
+}
 </script>
 
-
-<style>
-  canvas{
-			border: 2px solid black;
-			border-radius: 2px;
-			background-color: white ;
-		}
-
-		#hidden{
-		display: none ;
-		}
+<style scoped>
+canvas {
+	border: 2px solid black;
+	border-radius: 2px;
+	background-color: white;
+}
 </style>
